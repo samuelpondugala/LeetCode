b27e816b673c0f7424e12a71de9ab8db98846a6d
David Samuel <159622350+samuelpondugala@users.noreply.github.com>
2025-07-24 22:25:37 +0530

    Time: 48 ms (97.23%) | Memory: 18.1 MB (21.86%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -1,17 +1,22 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        res = []
-        i = 0
-        j = n
-        while i<n and j<len(nums):
-            res.append(nums[i])
-            res.append(nums[j])
-            i+=1
-            j+=1
-        while i<n:
-            res.append(nums[i])
-            i+=1
-        while j<len(nums):
-            res.append(nums[j])
-            j+=1
-        return res
+        # res = []
+        # i = 0
+        # j = n
+        # while i<n and j<len(nums):
+        #     res.append(nums[i])
+        #     res.append(nums[j])
+        #     i+=1
+        #     j+=1
+        # while i<n:
+        #     res.append(nums[i])
+        #     i+=1
+        # while j<len(nums):
+        #     res.append(nums[j])
+        #     j+=1
+        # return res
+        ans = []
+        for i in range(n):
+            ans += [nums[i]] 
+            ans += [nums[i+n]]
+        return ans
