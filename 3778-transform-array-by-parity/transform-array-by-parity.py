48cc1306f0a439706dd63216aaf3410c35c16664
David Samuel <159622350+samuelpondugala@users.noreply.github.com>
2025-07-24 16:03:29 +0530

    Time: 1 ms (43.45%) | Memory: 17.8 MB (81.47%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.py
+++ 3778-transform-array-by-parity/transform-array-by-parity.py
@@ -1,8 +1,14 @@
 class Solution:
     def transformArray(self, nums: List[int]) -> List[int]:
+        #x, y = 0
         for i in range(len(nums)):
             if nums[i]%2==0:
                 nums[i] = 0
+                #x+=1
             else:
                 nums[i]= 1
-        return sorted(nums)
+                #y+=1
+        #zeroes = [0 for i in range(x)]
+        #ones = [1 for i in range(y)]
+        #nums = zeroes + ones
+        return sorted(nums) # return nums
