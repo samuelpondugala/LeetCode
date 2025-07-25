9de00bf38369796c4fbfdd2b234878dc233256ad
David Samuel <159622350+samuelpondugala@users.noreply.github.com>
2025-07-25 10:29:51 +0530

    Time: 50 ms (74.10%) | Memory: 51 MB (86.50%) - LeetSync

--- 3541-report-spam-message/report-spam-message.py
+++ 3541-report-spam-message/report-spam-message.py
@@ -1,10 +1,10 @@
 class Solution:
     def reportSpam(self, message: List[str], bannedWords: List[str]) -> bool:
-        banned = set(bannedWords)
-        count = 0
-        for word in message:
-            if word in banned:
-                count += 1
-                if count >= 2:
+        b = set(bannedWords)
+        c = 0
+        for i in message:
+            if i in b:
+                c += 1
+                if c > 1:
                     return True
         return False
