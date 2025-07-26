07a3923012d53480cdca5c24d015237a665b1325
David Samuel <159622350+samuelpondugala@users.noreply.github.com>
2025-07-26 22:36:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (15.18%) - LeetSync

---
+++ 290-word-pattern/word-pattern.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordPattern(self, s: str, l: str) -> bool:
+        t = l.split()
+        f1 = {}
+        b1 = {}
+        f2 = {}
+        b2 = {}
+        if len(s)==len(t):
+            for i in range(len(s)):
+                f1[s[i]] = t[i]
+            for i in range(len(s)-1, -1, -1):
+                b1[s[i]] = t[i]
+            for i in range(len(s)):
+                f2[t[i]] = s[i]
+            for i in range(len(s)-1, -1, -1):
+                b2[t[i]] = s[i]
+            return (f1==b1 and f2==b2) and (len(set(s))==len(set(t)))
+        else:
+            return False
+    
