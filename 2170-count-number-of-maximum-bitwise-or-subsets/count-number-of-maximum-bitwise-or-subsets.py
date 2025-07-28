f040f24d2109b0dd4d0e9faa932dbffa63ddf4d8
David Samuel <159622350+samuelpondugala@users.noreply.github.com>
2025-07-28 15:07:20 +0530

    Time: 7 ms (98.78%) | Memory: 17.9 MB (40.33%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
@@ -1,25 +1,7 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
-        res = []
-        ans = []
-        def dfs(i):
-            if i>=len(nums):
-                res.append(ans.copy())
-                return
-            #decision to include nums[i]
-            ans.append(nums[i])
-            dfs(i+1)
-            #decision to exclude nums[i]
-            ans.pop()
-            dfs(i+1)
-        dfs(0)
-        summ = []
-        def xor(res):
-            for num in res:
-                xor_sum = 0
-                for i in num:
-                    xor_sum |= i  # Bitwise XOR operation
-                summ.append(xor_sum)
-        xor(res)
-        maxx = max(summ)
-        return summ.count(maxx)
+        prevBits = Counter([0])
+        for num in nums:
+            for prev, count in list(prevBits.items()):
+                prevBits[prev | num] += count
+        return prevBits[reduce(lambda a,b: a | b, nums)]
