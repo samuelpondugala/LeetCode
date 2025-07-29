931ddb026cb489a38e46ef042361efb1b88188cc
David Samuel <159622350+samuelpondugala@users.noreply.github.com>
2025-07-29 08:55:19 +0530

    Time: 102 ms (100.00%) | Memory: 29.8 MB (46.46%) - LeetSync

--- 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.py
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.py
@@ -1,20 +1,33 @@
+# class Solution:
+#     def smallestSubarrays(self, nums: List[int]) -> List[int]:
+        
+#         n = len(nums)
+#         rightmost = [0] * 30  # Track farthest index where bit is set
+#         res = [0] * n
+
+#         for i in range(n - 1, -1, -1):
+#             for b in range(30):  # Loop over all 30 bit positions
+#                 if nums[i] & (1 << b):
+#                     rightmost[b] = i  # Update where this bit is last seen
+
+#             # The max index needed to preserve current OR
+#             max_len = i
+#             for j in range(30):
+#                 max_len = max(max_len, rightmost[j])
+
+#             res[i] = max_len - i + 1  # Length of subarray
+
+#         return res
 class Solution:
     def smallestSubarrays(self, nums: List[int]) -> List[int]:
-        
-        n = len(nums)
-        rightmost = [0] * 30  # Track farthest index where bit is set
-        res = [0] * n
-
-        for i in range(n - 1, -1, -1):
-            for b in range(30):  # Loop over all 30 bit positions
-                if nums[i] & (1 << b):
-                    rightmost[b] = i  # Update where this bit is last seen
-
-            # The max index needed to preserve current OR
-            max_len = i
-            for j in range(30):
-                max_len = max(max_len, rightmost[j])
-
-            res[i] = max_len - i + 1  # Length of subarray
-
+        length = len(nums)
+        res = [inf] * length
+        for i in range(length):
+            x = nums[i]
+            res[i] = 1
+            j = i - 1
+            while j >= 0 and nums[j] | x != nums[j]:
+                res[j] = i - j + 1
+                nums[j] |= x
+                j -= 1
         return res
