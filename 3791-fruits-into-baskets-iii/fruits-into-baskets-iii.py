e55c31f73e5f900fda85b33dc57a3a27d32fefc3
David Samuel <159622350+samuelpondugala@users.noreply.github.com>
2025-08-06 09:19:07 +0530

    Time: 1875 ms (82.55%) | Memory: 35.7 MB (97.32%) - LeetSync

--- 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.py
+++ 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.py
@@ -1,57 +1,61 @@
-import math
-from typing import List
+class SegmentTree:
+    def __init__(self, data):
+        self.n = len(data)
+        # next power of two
+        size = 1
+        while size < self.n:
+            size <<= 1
+        self.size = size
+        self.tree = [0] * (2 * size)
+        # build leaves
+        for i, v in enumerate(data):
+            self.tree[size + i] = v
+        # build internal nodes
+        for i in range(size - 1, 0, -1):
+            self.tree[i] = max(self.tree[2*i], self.tree[2*i+1])
+
+    def update(self, idx, value):
+        # point update: set data[idx] = value
+        i = self.size + idx
+        self.tree[i] = value
+        i //= 2
+        while i:
+            self.tree[i] = max(self.tree[2*i], self.tree[2*i+1])
+            i //= 2
+
+    def find_first(self, node, node_lo, node_hi, fruit):
+        """
+        Finds the smallest index j in [node_lo, node_hi)
+        whose value >= fruit, or returns -1 if none.
+        """
+        if self.tree[node] < fruit:
+            return -1
+        if node_lo + 1 == node_hi:
+            # leaf
+            return node_lo
+        mid = (node_lo + node_hi) // 2
+        left_node = 2 * node
+        # try left child
+        if self.tree[left_node] >= fruit:
+            return self.find_first(left_node, node_lo, mid, fruit)
+        else:
+            return self.find_first(left_node + 1, mid, node_hi, fruit)
+
+    def query_first(self, fruit):
+        # API: search whole range [0, n)
+        return self.find_first(1, 0, self.size, fruit)
+
 
 class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
-        sect_mx = []
-        m = len(baskets)
-        a = int(math.sqrt(m))  # size of one array
-
-        cnt = 0
-        mx = 0
-        for i in range(m):
-            if cnt == a:
-                # creating sector of size a
-                sect_mx.append(mx)
-                mx = baskets[i]
-                cnt = 1
-            else:
-                cnt += 1
-                mx = max(mx, baskets[i])
-
-        # last sector
-        sect_mx.append(mx)
-
-        remain = 0
-
-        # start allocating
+        st = SegmentTree(baskets)
+        unplaced = 0
         for fruit in fruits:
-            k = 0
-            set_flag = 1
-
-            for j in range(0, m, a):
-                if sect_mx[k] < fruit:
-                    # skip this segment
-                    k += 1
-                    continue
-
-                # find place to allocate
-                for r in range(j, min(j + a, m)):
-                    if baskets[r] >= fruit:
-                        set_flag = 0
-                        baskets[r] = 0
-                        break
-
-                # if fruit is allocated in a sector
-                if set_flag == 0:
-                    sect_mx[k] = 0  # find new mx
-                    # update new sector mx
-                    for r in range(j, min(j + a, m)):
-                        sect_mx[k] = max(baskets[r], sect_mx[k])
-                    break
-
-                k += 1
-
-            remain += set_flag
-
-        return remain
+            j = st.query_first(fruit)
+            if j == -1 or j >= len(baskets):
+                # no suitable basket
+                unplaced += 1
+            else:
+                # place it and mark basket j as used
+                st.update(j, 0)
+        return unplaced
