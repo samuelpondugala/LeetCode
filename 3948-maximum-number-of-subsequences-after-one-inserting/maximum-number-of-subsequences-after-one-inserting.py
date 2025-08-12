1b3806733b88a4e916b329a3ed42586c10bfc12b
David Samuel <159622350+samuelpondugala@users.noreply.github.com>
2025-08-12 09:25:47 +0530

    Time: 111 ms (100.00%) | Memory: 18.2 MB (82.38%) - LeetSync

--- 3948-maximum-number-of-subsequences-after-one-inserting/maximum-number-of-subsequences-after-one-inserting.py
+++ 3948-maximum-number-of-subsequences-after-one-inserting/maximum-number-of-subsequences-after-one-inserting.py
@@ -1,31 +1,19 @@
+# https://www.youtube.com/@0x3f
 class Solution:
     def numOfSubsequences(self, s: str) -> int:
-        n = len(s)
-        prefix = [0] * (n + 1)  # `L` count (1-based)
-        suffix = [0] * (n + 1)  # `T` count (0-based)
-
-        for i in range(n):
-            if s[i] == 'L':
-                prefix[i + 1] = 1
-            prefix[i + 1] += prefix[i]
-
-        for i in range(n - 1, -1, -1):
-            if s[i] == 'T':
-                suffix[i] = 1
-            suffix[i] += suffix[i + 1]
-
-        resWithC = 0
-        bestPosForC = 0
-        resWithL = 0
-        resWithT = 0
-
-        for i in range(n):
-            if s[i] == 'C':
-                resWithC += prefix[i] * suffix[i + 1]
-                resWithL += (prefix[i] + 1) * suffix[i + 1]
-                resWithT += prefix[i] * (suffix[i + 1] + 1)
-            else:
-                bestPosForC = max(bestPosForC, prefix[i] * suffix[i])
-
-        resWithC += bestPosForC
-        return max(resWithL, resWithT, resWithC)
+        t = s.count('T')
+        l = lc = lct = c = ct = lt = 0
+        for b in s:
+            if b == 'L':
+                l += 1
+            elif b == 'C':
+                lc += l
+                c += 1
+            elif b == 'T':
+                lct += lc
+                ct += c
+                t -= 1
+            v = l * t
+            if v > lt:
+                lt = v
+        return lct + max(ct, lc, lt)
