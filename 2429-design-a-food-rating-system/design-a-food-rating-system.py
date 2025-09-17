79727f6193f3d4cf0dc96c3f6b250b59867f8ac5
David Samuel <159622350+samuelpondugala@users.noreply.github.com>
2025-09-17 16:09:31 +0530

    Time: 118 ms (63.06%) | Memory: 51.9 MB (58.05%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.py
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.py
@@ -1,43 +1,35 @@
+import heapq
 from collections import defaultdict
-class FoodRatings(object):
 
-    def __init__(self, foods, cuisines, ratings):
-        """
-        :type foods: List[str]
-        :type cuisines: List[str]
-        :type ratings: List[int]
-        """
-        self.cuisine_to_heap = defaultdict(list)
-        self.food_to_cuisine = {}
-        self.food_to_rating = defaultdict(int)
-        for i in range(len(foods)):
-            self.food_to_cuisine[foods[i]] = cuisines[i]
-            heapq.heappush(self.cuisine_to_heap[cuisines[i]], (-ratings[i], foods[i]))
-            self.food_to_rating[foods[i]] = -ratings[i]
+class FoodRatings:
+
+    def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
+        self.foods = {}
+        self.rated_cuisine = defaultdict(list)
+
+        for food, cuisine, rating in zip(foods, cuisines, ratings):
+            self.foods[food] = (rating, cuisine)
+            self.rated_cuisine[cuisine].append((-rating, food))
+        
+        for cuisine in self.rated_cuisine:
+            heapq.heapify(self.rated_cuisine[cuisine])
+
+
+    def changeRating(self, food: str, newRating: int) -> None:
+        _, curr_cuisine = self.foods[food]
+        self.foods[food] = (newRating, curr_cuisine)
+        heapq.heappush(self.rated_cuisine[curr_cuisine], (-newRating, food))
+
+
+    def highestRated(self, cuisine: str) -> str:
+        while self.rated_cuisine[cuisine][0][0] != -self.foods[self.rated_cuisine[cuisine][0][1]][0]:
+            heapq.heappop(self.rated_cuisine[cuisine])
+        return self.rated_cuisine[cuisine][0][1]
+
         
 
-    def changeRating(self, food, newRating):
-        """
-        :type food: str
-        :type newRating: int
-        :rtype: None
-        """
-        cuisine = self.food_to_cuisine[food]
-        heapq.heappush(self.cuisine_to_heap[cuisine], (-newRating, food))
-        self.food_to_rating[food] = -newRating
 
-    def highestRated(self, cuisine):
-        """
-        :type cuisine: str
-        :rtype: str
-        """
-        smallest_lexico = None
-        while len(self.cuisine_to_heap[cuisine]) > 0:
-            curr = self.cuisine_to_heap[cuisine][0]
-            if curr[0] != self.food_to_rating[curr[1]]:
-                # delete old data
-                heapq.heappop(self.cuisine_to_heap[cuisine])
-                continue
-            smallest_lexico = curr[1]
-            break
-        return smallest_lexico
+# Your FoodRatings object will be instantiated and called as such:
+# obj = FoodRatings(foods, cuisines, ratings)
+# obj.changeRating(food,newRating)
+# param_2 = obj.highestRated(cuisine)
