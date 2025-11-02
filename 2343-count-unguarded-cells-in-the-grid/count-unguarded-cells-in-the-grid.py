e8654f2b518a9aaffddffff1acea00583b699ee2
David Samuel <159622350+samuelpondugala@users.noreply.github.com>
2025-11-02 12:26:40 +0530

    Time: 188 ms (99.49%) | Memory: 39.1 MB (68.37%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.py
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.py
@@ -1,50 +1,30 @@
 class Solution:
-    UNGUARDED = 0
-    GUARDED = 1
-    GUARD = 2
-    WALL = 3
-
-    def countUnguarded(
-        self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]
-    ) -> int:
-        grid = [[self.UNGUARDED] * n for _ in range(m)]
-        for row, col in guards:
-            grid[row][col] = self.GUARD
-        for row, col in walls:
-            grid[row][col] = self.WALL
-        def _updatecell_visibility(row, col, is_guard_line_active):
-            if grid[row][col] == self.GUARD:
-                return True
-            if grid[row][col] == self.WALL:
-                return False
-            if is_guard_line_active:
-                grid[row][col] = self.GUARDED
-            return is_guard_line_active
-        for row in range(m):
-            is_guard_line_active = grid[row][0] == self.GUARD
-            for col in range(1, n):
-                is_guard_line_active = _updatecell_visibility(
-                    row, col, is_guard_line_active
-                )
-            is_guard_line_active = grid[row][n - 1] == self.GUARD
-            for col in range(n - 2, -1, -1):
-                is_guard_line_active = _updatecell_visibility(
-                    row, col, is_guard_line_active
-                )
-        for col in range(n):
-            is_guard_line_active = grid[0][col] == self.GUARD
-            for row in range(1, m):
-                is_guard_line_active = _updatecell_visibility(
-                    row, col, is_guard_line_active
-                )
-            is_guard_line_active = grid[m - 1][col] == self.GUARD
-            for row in range(m - 2, -1, -1):
-                is_guard_line_active = _updatecell_visibility(
-                    row, col, is_guard_line_active
-                )
-        count = 0
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col] == self.UNGUARDED:
-                    count += 1
-        return count
+    def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+        grid = [[0]*n for _ in range(m)]
+        for r,c in walls:
+            grid[r][c] = 1
+        for r,c in guards:
+            grid[r][c] = 1
+        for r,c in guards:
+            u = r-1
+            while u >= 0 and grid[u][c] != 1:
+                grid[u][c] = 2
+                u -= 1
+            d = r+1
+            while d < m and grid[d][c] != 1:
+                grid[d][c] = 2
+                d += 1
+            l = c-1
+            while l >= 0 and grid[r][l] != 1:
+                grid[r][l] = 2
+                l -= 1
+            ri = c+1
+            while ri < n and grid[r][ri] != 1:
+                grid[r][ri] = 2
+                ri += 1
+        unguarded = 0
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == 0:
+                    unguarded += 1
+        return unguarded
