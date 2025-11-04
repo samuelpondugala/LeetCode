51e58bf79e16665d2eb237f43239cf5c7f0a6fd4
David Samuel <159622350+samuelpondugala@users.noreply.github.com>
2025-11-04 09:43:16 +0530

    Time: 22 ms (78.18%) | Memory: 17.8 MB (59.74%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
@@ -1,12 +1,26 @@
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
+        k_ele_maps = {}
+        answer = [0] * (len(nums) - k + 1)
 
-        def do_sum (idx: int)-> int:
+        for i in range(k):
+            if nums[i] not in k_ele_maps:
+                k_ele_maps[nums[i]] = 1
+            else:
+                k_ele_maps[nums[i]] += 1
 
-            ctr = Counter(nums[idx:idx + k])
-            most_freq = nlargest(x, ctr, key = lambda y: (ctr[y], y))
+        for i in range(len(nums) - k + 1):
+            top = sorted(k_ele_maps.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)[:x]
+            answer[i] = sum(v * f for v, f in top)
+            out_num = nums[i]
+            k_ele_maps[out_num] -= 1
+            if k_ele_maps[out_num] == 0:
+                del k_ele_maps[out_num]
+            if i + k < len(nums):
+                in_num = nums[i + k]
+                if in_num not in k_ele_maps:
+                    k_ele_maps[in_num] = 1
+                else:
+                    k_ele_maps[in_num] += 1
 
-            return sum(list(map(lambda y: y * ctr[y], most_freq)))
-
-
-        return list(map(do_sum,range(len(nums)+1 - k)))
+        return(answer)  
